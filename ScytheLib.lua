38cf3c3e5bbf2840e62076366497e10cf9c39fc3
pisqstroY <143507753+pisqstroY@users.noreply.github.com>
2026-04-30 01:58:05 +0300

    Update ScytheLib.lua

--- ScytheLib.lua
+++ ScytheLib.lua
@@ -2942,14 +2942,14 @@ function Library:CreateWindow(...)
         BorderColor3 = 'AccentColor';
     });
 
-    local WindowLabel = Library:CreateLabel({
-        Position = UDim2.new(0, 7, 0, 0);
-        Size = UDim2.new(0, 0, 0, 25);
-        Text = Config.Title or '';
-        TextXAlignment = Enum.TextXAlignment.Left;
-        ZIndex = 1;
-        Parent = Inner;
-    });
+local WindowLabel = Library:CreateLabel({
+    Position = UDim2.new(0, 0, 0, 0);
+    Size = UDim2.new(1, 0, 0, 25);
+    Text = Config.Title or '';
+    TextXAlignment = Enum.TextXAlignment.Center;
+    ZIndex = 1;
+    Parent = Inner;
+});
 
     local MainSectionOuter = Library:Create('Frame', {
         BackgroundColor3 = Library.BackgroundColor;
