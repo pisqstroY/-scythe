0ff7fbfec8a77635b7c2756e9a27ee342e7ca2f2
pisqstroY <143507753+pisqstroY@users.noreply.github.com>
2026-07-02 04:56:13 +0300

    Update FemWareUI.lua

--- FemWareUI.lua
+++ FemWareUI.lua
@@ -185,7 +185,7 @@ local function GetCustomAsset(name, url)
 end
 
 
-local BannerAsset = GetCustomAsset("FemBanner.jpg", "https://raw.githubusercontent.com/pisqstroY/-scythe/refs/heads/main/fate33.png")
+local BannerAsset = GetCustomAsset("FemBanner.jpg", "https://raw.githubusercontent.com/pisqstroY/-scythe/refs/heads/main/new%20fate-Photoroom.png")
 
 local Library do
     local Workspace = game:GetService("Workspace")
